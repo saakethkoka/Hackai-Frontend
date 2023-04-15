ad62d099324f989dd8b4fbced71a4e9643fb03c7
RayaanJIrani <rayaanjirani@gmail.com>
2023-04-15 18:06:36 -0500

    Added basic examples for APi

--- src/api/UserApi.jsx
+++ src/api/UserApi.jsx
@@ -1,2 +1,14 @@
 //TODO: this is where the API is going to be setup
 import axios from 'axios';
+
+const baseEndpoint = '3.139.106.132:8080/api';
+
+export const questions = async () => {
+    const response = await axios.post(`${baseEndpoint}/questions`);
+    return response.data;
+}
+
+export const image = async () => {
+    const response = await axios.post(`${baseEndpoint}/image`);
+    return response.data;
+}
