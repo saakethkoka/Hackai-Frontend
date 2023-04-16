b7d399ea0ad8c62510ea513779af0f345926caab
EliasLMann <70070619+EliasLMann@users.noreply.github.com>
2023-04-16 13:32:04 -0500

    fixed submit enter

--- src/components/QuestionsAndAnswerBox.jsx
+++ src/components/QuestionsAndAnswerBox.jsx
@@ -1,38 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import Question from './Question';
-import { Button } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import Question from "./Question";
+import { Button } from "@mui/material";
 
-export const QuestionsAndAnswerBox = ({ questions, setResponses, responses, handleSubmit}) => {
-
-
-    const handleResponse = (questionIndex, choice) => {
-        const updatedResponses = [...responses];
-        updatedResponses[questionIndex] = {
-            question: questions[questionIndex].question,
-            response: choice,
-        };
-        setResponses(updatedResponses);
+export const QuestionsAndAnswerBox = ({
+  questions,
+  setResponses,
+  responses,
+  handleSubmit,
+}) => {
+  const handleResponse = (questionIndex, choice) => {
+    const updatedResponses = [...responses];
+    updatedResponses[questionIndex] = {
+      question: questions[questionIndex].question,
+      response: choice,
     };
+    setResponses(updatedResponses);
+  };
 
-    
+  const handleKeyPress = (event) => {
+    if (event.which === 13 || event.keyCode === 13) {
+      event.preventDefault();
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            {questions.map((question, index) => (
-                <Question
-                    key={index}
-                    question={question.question}
-                    choices={question.choices}
-                    index={index}
-                    response={responses[index]?.response}
-                    handleResponse={handleResponse}
-                />
-            ))}
-            <Button variant="contained" color="primary" type="submit" style={{ margin: 'auto', display: 'block' }}>
-                Submit
-            </Button>
-
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit} onKeyPress={handleKeyPress}>
+      {questions.map((question, index) => (
+        <Question
+          key={index}
+          question={question.question}
+          choices={question.choices}
+          index={index}
+          response={responses[index]?.response}
+          handleResponse={handleResponse}
+        />
+      ))}
+      <Button
+        variant="contained"
+        color="primary"
+        type="submit"
+        style={{ margin: "auto", display: "block" }}
+      >
+        Submit
+      </Button>
+    </form>
+  );
 };
-
