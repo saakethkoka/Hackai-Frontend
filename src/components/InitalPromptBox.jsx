23b1eb3f32b0ba04384a423a94c72bf1f7cf3d3b
saakethkoka <63078934+saakethkoka@users.noreply.github.com>
2023-04-15 19:46:13 -0500

    works

--- src/components/InitalPromptBox.jsx
+++ src/components/InitalPromptBox.jsx
@@ -1,4 +1,5 @@
-import { TextField } from '@material-ui/core';
+import TextField from '@mui/material/TextField'
+
 export const InitalPromptBox = ({placeholder = "", }) => {
     return (
         <TextField id="standard-basic" label="Standard" variant="standard" />
