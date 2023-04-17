f2501354ea6dc2c78bc457e849ce8bc9ebc6e1f5
RayaanJIrani <rayaanjirani@gmail.com>
2023-04-17 12:50:43 -0500

    Updated the 404 Error page to be much nicer

--- src/404/Page404.jsx
+++ src/404/Page404.jsx
@@ -1,18 +1,38 @@
 
+import React from 'react';
+import { Box, Button, Container, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+
 export const Page404 = () => {
     return (
-        <>
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="text-center mt-5">
-                        <h1 className="display-1 fw-bold text-light">404</h1>
-                        <p className="h1 text-light">Page Not Found</p>
-                        <p className="h2 text-light">The page you requested was not found.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </>
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                minHeight: '100vh'
+            }}
+        >
+            <Container maxWidth="md">
+                <Grid container spacing={2}>
+                    <Grid xs={6}>
+                        <Typography variant="h1">
+                            404
+                        </Typography>
+                        <Typography variant="h6">
+                            The page you’re looking for doesn’t exist.
+                        </Typography>
+                        <Button variant="contained" href={"/"}>Back Home</Button>
+                    </Grid>
+                    <Grid xs={6}>
+                        <img
+                            src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569__340.jpg"
+                            alt=""
+                            width={500} height={250}
+                        />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
     );
 }
